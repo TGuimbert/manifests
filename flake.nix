693530105fa0744dd3d94ceeb336a7c54500b0eb
TGuimbert <33598842+TGuimbert@users.noreply.github.com>
2023-04-28 13:05:47 +0200

    Add FluxCD CLI tools to development environment

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
                   packages = with pkgs; [
                     kyverno
                     kustomize
+                    fluxcd
                     gnumake
                   ];
 
